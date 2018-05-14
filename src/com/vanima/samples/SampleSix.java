f3bd08dcb79bea9f0d7600283ce6e95bf4fc436c
--show-origin <prasadved@hotmail.com>
2018-05-14 11:32:35 +0530

    sample five and six modified

--- src/com/vanima/samples/SampleSix.java
+++ src/com/vanima/samples/SampleSix.java
@@ -2,8 +2,36 @@ package com.vanima.samples;
 
 public class SampleSix {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Sample Six");
+	public static void main(String[] args) throws InterruptedException {
+		String str1 = new String("java");   // Object 
+		String str2 = new String("gft");    // object 
+		
+		String str3 = str1 ;   //reference 
+		
+		str1 = str1.concat(str2); // new object created and assigned to str1  
+		str3 = str2.toUpperCase(); //new object got created and assigned to str3  
+		 
+		str3 = str2 ;  //reference 
+		
+		/*System.out.println("going into sleep mode");
+		Thread.sleep(60000 * 3);
+		System.out.println("coming out of sleep mode");*/
+		
+		String str4 = str3 ;
+		String str5 = new String("Hello World!!!");
+		System.out.println(str5);
+		//stest();
 	}
-
+	
+	public static void test() {
+		String s1 = "java" ;
+		String s2 = s1 ;
+		String s3 = "gft" ;
+		s1.concat("world");   //garbage collection 
+		String s4 = new String("guys");   
+		s2 = null ;  //garbage collection 
+		
+		System.out.println(s1);
+		
+	}
 }
