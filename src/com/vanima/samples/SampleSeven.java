98d3f3f1df995c4a27c4ed0b87b763b989da66a7
--show-origin <prasadved@hotmail.com>
2018-05-14 14:19:45 +0530

    sampleseven added from develop branch

---
+++ src/com/vanima/samples/SampleSeven.java
@@ -0,0 +1,16 @@
+package com.vanima.samples;
+
+public class SampleSeven {
+
+	public static void main(String[] args) {
+		
+		char ch = 'B' ;
+		
+		int val ;
+		
+		val = ch != 0 ? ch : 'A';
+		
+		System.out.println("Value " + (char)val);
+	}
+
+}
